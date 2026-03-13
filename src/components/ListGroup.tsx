f15afb42b602762bee7d7990606606690e74e7c6
Jannis Häffner <jannis.haeffner@yahoo.de>
2026-03-14 00:22:44 +0100

    chore: initial project setup

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,11 @@
+function ListGroup() {
+  return (
+    <div className="card p-3 shadow-sm">
+      <h5>Weather</h5>
+      <p>Stockholm</p>
+      <p>5°C</p>
+    </div>
+  );
+}
+
+export default ListGroup;
